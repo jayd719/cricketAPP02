fbdc233c5531dcf3416e0958cd3f8e8e5a64cef9
JD-MBP <jashansingh29@gmail.com>
2024-07-05 17:00:10 -0400

    ;

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 import sys
 
+
 sys.path.insert(0,'/home/ubuntu/cricketAPP02')
 activate_this ='/home/ubuntu/cricketAPP02/env/bin/active_this.py'
 with open(activate_this) as file_:
-    exec(file_.read(),dict(__file__=activate_this))
-
+    exec(file_.read(), dict(__file__=activate_this))
+    
 from mainFile import app as application
